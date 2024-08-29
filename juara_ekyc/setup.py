327f5de600acd6c691468667d3893586c9042db3
juaraprabhjeevan <prabhjeevan@juarapartners.com>
2024-08-29 14:02:44 +0800

    Latest wheel built

--- juara_ekyc/setup.py
+++ juara_ekyc/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="juara_ekyc",
-    version="0.1.0",
+    version="0.0.1",
     author="Prabhjeevan Singh",
     author_email="prabhjeevan@juarapartners.com",
     description="A library for electronic Know Your Customer (eKYC) verification",
