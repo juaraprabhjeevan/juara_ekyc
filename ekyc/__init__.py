03c3bfaeec709a9219117c9b5f253fe5242bcdc1
juaraprabhjeevan <prabhjeevan@juarapartners.com>
2024-09-04 17:57:47 +0800

    Update liveness check with improved logging and error handling

--- ekyc/__init__.py
+++ ekyc/__init__.py
@@ -20,4 +20,4 @@ __all__ = [
     'setup_logger'
 ]
 
-__version__ = "0.1.11" 
+__version__ = "0.1.12" 
--- ekyc/test.py
+++ ekyc/test.py
@@ -1,5 +1,5 @@
 from ekyc import process_id_verification
 import json
 
-result = process_id_verification('/Users/prabhjeevan/Documents/Data_Science/juara_ekyc/UserFaces/prabh1.jpg')
+result = process_id_verification('/Users/prabhjeevan/Documents/Data_Science/Facial_Matching/UserFaces/prabh1.jpg')
 print(result)
